4ce2ff007aabcea5fce449b031100c4eb3f7b6b2
karaage0703 <coldchicken@gmail.com>
2019-05-14 02:14:30 +0900

    Add script for jetson nano

---
+++ scripts/convert_rt_model.py
@@ -0,0 +1,36 @@
+# coding: utf-8
+import argparse
+from PIL import Image
+import sys
+import os
+import tensorflow.contrib.tensorrt as trt
+import tensorflow as tf
+import numpy as np
+import time
+from tf_trt_models.detection import download_detection_model, build_detection_graph
+
+# Path to label and frozen detection graph. This is the actual model that is used for the object detection.
+parser = argparse.ArgumentParser(description='convert_rt_model.')
+parser.add_argument('-c', '--config', default='./parallels.config')
+parser.add_argument('-m', '--model', default='./model.ckp')
+parser.add_argument('-o', '--output', default='./exported_graphs/frozen_inference_graph_trt.pb')
+
+args = parser.parse_args()
+
+frozen_graph, input_names, output_names = build_detection_graph(
+    config=args.config,
+    checkpoint=args.model,
+    score_threshold=0.3,
+    batch_size=1
+
+trt_graph = trt.create_inference_graph(
+    input_graph_def=frozen_graph,
+    outputs=output_names,
+    max_batch_size=1,
+    max_workspace_size_bytes=1 << 25,
+    precision_mode='FP16',
+    minimum_segment_size=50
+)
+
+with open(args.output, 'wb') as f:
+    f.write(trt_graph.SerializeToString())
---
+++ scripts/object_detection_tutorial_jetson_nano.py
@@ -0,0 +1,146 @@
+# coding: utf-8
+# Object Detection Demo
+import argparse
+import cv2
+import numpy as np
+import os
+import six.moves.urllib as urllib
+import sys
+import tarfile
+import tensorflow as tf
+import zipfile
+
+from distutils.version import StrictVersion
+from collections import defaultdict
+from io import StringIO
+from matplotlib import pyplot as plt
+from PIL import Image
+
+from object_detection.utils import ops as utils_ops
+
+if StrictVersion(tf.__version__) < StrictVersion('1.12.0'):
+  raise ImportError('Please upgrade your TensorFlow installation to v1.12.*.')
+
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+
+# Path to label and frozen detection graph. This is the actual model that is used for the object detection.
+parser = argparse.ArgumentParser(description='object_detection_tutorial.')
+parser.add_argument('-l', '--labels', default='./object_detection_tools/data/tf_label_map.pbtxt')
+parser.add_argument('-m', '--model', default='./exported_graphs/frozen_inference_graph.pb')
+
+args = parser.parse_args()
+
+# Load a (frozen) Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+  od_graph_def = tf.GraphDef()
+  with tf.gfile.GFile(args.model, 'rb') as fid:
+    serialized_graph = fid.read()
+    od_graph_def.ParseFromString(serialized_graph)
+    tf.import_graph_def(od_graph_def, name='')
+
+category_index = label_map_util.create_category_index_from_labelmap(args.labels, use_display_name=True)
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+def run_inference_for_single_image(image, graph):
+  with graph.as_default():
+    with tf.Session() as sess:
+      # Get handles to input and output tensors
+      ops = tf.get_default_graph().get_operations()
+      all_tensor_names = {output.name for op in ops for output in op.outputs}
+      tensor_dict = {}
+      for key in [
+          'num_detections', 'detection_boxes', 'detection_scores',
+          'detection_classes', 'detection_masks'
+      ]:
+        tensor_name = key + ':0'
+        if tensor_name in all_tensor_names:
+          tensor_dict[key] = tf.get_default_graph().get_tensor_by_name(
+              tensor_name)
+      if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(detection_masks, [0, 0, 0], [real_num_detection, -1, -1])
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[1], image.shape[2])
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8)
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(
+            detection_masks_reframed, 0)
+      image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+      # Run inference
+      output_dict = sess.run(tensor_dict,
+                             feed_dict={image_tensor: image})
+
+      # all outputs are float32 numpy arrays, so convert types as appropriate
+      output_dict['num_detections'] = int(output_dict['num_detections'][0])
+      output_dict['detection_classes'] = output_dict[
+          'detection_classes'][0].astype(np.int64)
+      output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+      output_dict['detection_scores'] = output_dict['detection_scores'][0]
+      if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+  return output_dict
+
+
+GST_STR = 'nvarguscamerasrc \
+    ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=(fraction)30/1 \
+    ! nvvidconv ! video/x-raw, width=(int)1920, height=(int)1080, format=(string)BGRx \
+    ! videoconvert \
+    ! appsink'
+cam = cv2.VideoCapture(GST_STR, cv2.CAP_GSTREAMER) # Raspi cam
+# cam = cv2.VideoCapture(1) # USB Cam
+
+count_max = 30
+
+if __name__ == '__main__':
+    count = 0
+    while True:
+        ret, img = cam.read()
+        if not ret:
+            print('error')
+            break
+        # cv2.imshow('keras-pi inspector', capture)
+        key = cv2.waitKey(1)
+        if key == 27: # when ESC key is pressed break
+            break
+
+        count += 1
+
+        if count == count_max:
+            img_bgr = cv2.resize(img, (300, 300))
+
+            # convert bgr to rgb
+            image_np = img_bgr[:,:,::-1]
+            image_np_expanded = np.expand_dims(image_np, axis=0)
+            output_dict = run_inference_for_single_image(image_np_expanded, detection_graph)
+
+            # Visualization of the results of a detection.
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                image_np,
+                output_dict['detection_boxes'],
+                output_dict['detection_classes'],
+                output_dict['detection_scores'],
+                category_index,
+                instance_masks=output_dict.get('detection_masks'),
+                use_normalized_coordinates=True,
+                line_thickness=8)
+
+            # convert rgb to bgr
+            image_bgr = image_np[:,:,::-1]
+            cv2.imshow('detection result', image_bgr)
+            count = 0
+
+    cam.release()
+    cv2.destroyAllWindows()
