b528144a6549d7c018697dd134870d50f14c7d1c
karaage0703 <coldchicken@gmail.com>
2019-05-19 12:21:08 +0900

    Add mobilenet download script

---
+++ models/get_ssd_mobilenet_v1_coco_model.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget https://raw.githubusercontent.com/amikelive/coco-labels/master/coco-labels-paper.txt
+wget http://download.tensorflow.org/models/object_detection/ssd_mobilenet_v1_coco_2018_01_28.tar.gz
+tar xvzf ssd_mobilenet_v1_coco_2018_01_28.tar.gz
+rm ssd_mobilenet_v1_coco_2018_01_28.tar.gz
